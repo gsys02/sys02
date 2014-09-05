3a8ed7ebefdfb0ccf70f78a78113360a5b9b15b5
Sinuhe MB <gnu.ico@gmail.com>
2014-09-05 03:25:24 +0000

    Se agrega funcionalidad de spring web-flow security

---
+++ sys02-web/src/main/java/org/springframework/samples/webflow/validation/Account.java
@@ -0,0 +1,40 @@
+package org.springframework.samples.webflow.validation;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Account implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String firstName;
+
+	private String lastName;
+
+	private Date dateOfBirth;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+}
